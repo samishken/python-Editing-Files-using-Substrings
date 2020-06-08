965984f7d0f0a8ae69818476b9d0bc1debff3a42
Samuel Haile <samishken@gmail.com>
2020-06-08 17:19:39 -0400

    Create changeJane.py

---
+++ data/changeJane.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import sys
+import subprocess
+
+filename = sys.argv[1]
+
+with open(filename) as f:
+
+  for line in file.readlines():
+    string = line.strip()
+    name = "jane"
+    newname = string.replace(name,"jdoe")
+    subprocess.run(["mv", string, newname])
+
+f.close()
+
+with open(filename) as f:
+    
+    data = f.read().rstrip("\n").split("\n")
+    
+    for line in data:
+       source = ".." + line
+       print(source)
+       destination = source.replace("jane", "jdoe")
+
+       print("Source {} - Destination {} \n".format(line, destination))
+       subprocess.run(["mv", source, destination])
+f.close
+
+
+openfile = sys.argv[1]
+
+with open(openfile) as file:
+  for line in file.readlines():
+    mystr = line.strip()
+    oldname ="jane"
+    newname=mystr.replace(oldname,"jdoe")
+    #print(newname)
+    subprocess.run(["mv",mystr,newname ])
+file.close()
+
